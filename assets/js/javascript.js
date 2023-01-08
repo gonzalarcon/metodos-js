0d8306312a0cc6486c32b8f9aa1b62ef3bfe4b49
Gonza <goialarcon@gmail.com>
2023-01-08 13:31:23 -0300

    correcciones 1

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,51 +1,63 @@
+const btnAgregar = document.querySelector("#newTask")
+// const task = document.querySelector("#newTask")
 const listaDeTareas = document.querySelector("#tasks")
 const tareaInput = document.querySelector("#nuevaTarea")
-const btnAgregar = document.querySelector("#agregarTarea")
 const TareasTotal = document.querySelector("#totalTasks")
 const TareasRealizadas = document.querySelector("#checkTasks")
 const tasks = [];
 
-btnAgregar.addEventListener("click", ()=>{ 
-    if(tareaInput.value == ""){
+btnAgregar.addEventListener('click', ()=>{ 
+    if(tareaInput.value === ""){
         alert ("debes ingresar una tarea")
-        return
-    } else{
-        // const agregarTarea = () =>{
-        // }
-        const nuevaTarea = {
+        return;
+    }else{ 
+        agregarTarea();
+    }
+});
+
+const agregarTarea = () =>{
+    const nuevaTarea = {
             id: tasks.length + 1,
             nombre: tareaInput.value,
             status: false,
         } 
         tasks.push(nuevaTarea)
-        tareaInput.value="";
-        
-            let html = "";
-            for(let task of tasks){
-            html += `<tr>
-                        <td>${task.nombre}</td> 
-                        <td id="${task.id}">${task.id}</td>
-                        <td><input type="checkbox" id="checkbox" name="checkbox"></td>
-                        <td><button onclick="borrar(${task.id})" class="btn-danger">Borrar Tarea</button></td>
-                        </tr>`;
-            
-            listaDeTareas.innerHTML=html;
-            tareaInput.value='';
-            TareasTotal.innerHTML=tasks.length;
-            }   
+        updateList();
+};
 
-            
+const updateList = () =>{
+    let row = ""; countTaskReady=0;
+    for(let task of tasks){
+        if(nuevaTarea.status){
+            countTaskReady++;
+        }
+        row += 
+            `<tr class="${task.status ? 'bg-success' : 'bg-Light'}">
+                <td>${task.nombre}</td>
+                <td id="${task.id}">${task.id}</td>
+                <td class="text-right"> <button onclick"updateStatus(${task.id}" class="btn btn-${task.status ? 'success' : 'warning'}">${task.status ? 'Realizada' : 'Pendiente'}</button></td>
+                <td class="text-right"><button onclick="borrar(${task.id})" class="btn btn-danger" id="btnEliminar">Borrar Tarea</button></td>
+            </tr>`;
     }
-})
+            
+        listaDeTareas.innerHTML=row;
+        tareaInput.value='';
+        TareasTotal.innerHTML=tasks.length;
+        TareasRealizadas.innerHTML=countTaskReady;   
 
+}            
+
+const updateStatus=(taskId)=>{
+    const index = tareaInput.list.findIndex(task=>task.id===TaskId);
+    tareaInput[index].status= !tareaInput[index].status;
+}
 
 const borrar = (taskId) =>{
     const confirms = confirm('¿Seguro que deseas eliminar esta tarea?');
     if(confirms){
-        const index = tasks.findIndex(task=>task.id===taskId)
+        const index = tareaInput.list.findIndex(task=>task.id===TaskId)
         tasks.splice(index,1);  
+        updateList();
     };
-    const d = document.querySelector("#tasks")
-    d.remove();
 }
 
